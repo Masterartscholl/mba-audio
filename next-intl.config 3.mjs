c0ec73da4f36c43cdf0bc06ae8f007b98f20254a
Masterartscholl <crystalproduksiyon@gmail.com>
2026-02-25 01:26:27 +0300

    fix: project files update

--- next-intl.config 3.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-intl').NextIntlConfig} */
-const config = {
-  locales: ['tr', 'en'],
-  defaultLocale: 'tr'
-};
-
-export default config;
-
--- update_tracks.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.tracks ADD COLUMN IF NOT EXISTS artist_name text;
-ALTER TABLE public.tracks ADD COLUMN IF NOT EXISTS image_url text;
